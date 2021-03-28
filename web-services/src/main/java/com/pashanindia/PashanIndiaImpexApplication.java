00600f024640ecabae265ff94b640f96981d89a0
Anshuljain579 <jainanshul579gmail.com>
2021-03-28 15:45:53 +0530

    Initial Commit Base

---
+++ web-services/src/main/java/com/pashanindia/PashanIndiaImpexApplication.java
@@ -0,0 +1,13 @@
+package com.pashanindia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PashanIndiaImpexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PashanIndiaImpexApplication.class, args);
+	}
+
+}
---
+++ web-services/src/test/java/com/pashanindia/PashanIndiaImpexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pashanindia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PashanIndiaImpexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
